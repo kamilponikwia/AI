2cf6084725f0cebc7cbcb18827630b3c1a082305
lim4k <lim4k@DESKTOP-QIQF5OT>
2021-05-22 12:08:31 +0200

    implementacja klas oraz akcji dla kalkulatora kredytowego

--- app/templates_c/b4085398bd620dd21cd1d615ad4ae84fbf8658d4_0.file.main.tpl.php
+++ app/templates_c/b4085398bd620dd21cd1d615ad4ae84fbf8658d4_0.file.main.tpl.php
@@ -1,18 +1,18 @@
 <?php
-/* Smarty version 3.1.39, created on 2021-05-18 12:45:48
-  from 'D:\XAMPP\htdocs\php_kalkulator_kredytowy\templates\main.html' */
+/* Smarty version 3.1.39, created on 2021-05-22 12:05:33
+  from 'D:\XAMPP\htdocs\php_kalkulator_kredytowy\templates\main.tpl' */
 
 /* @var Smarty_Internal_Template $_smarty_tpl */
 if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   'version' => '3.1.39',
-  'unifunc' => 'content_60a39adcb54890_51683897',
+  'unifunc' => 'content_60a8d76d50c949_23950291',
   'has_nocache_code' => false,
   'file_dependency' => 
   array (
-    'c905285bb48c950535c410326c6cf68ef443e473' => 
+    'b4085398bd620dd21cd1d615ad4ae84fbf8658d4' => 
     array (
-      0 => 'D:\\XAMPP\\htdocs\\php_kalkulator_kredytowy\\templates\\main.html',
-      1 => 1621334744,
+      0 => 'D:\\XAMPP\\htdocs\\php_kalkulator_kredytowy\\templates\\main.tpl',
+      1 => 1621677914,
       2 => 'file',
     ),
   ),
@@ -20,7 +20,7 @@ if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   array (
   ),
 ),false)) {
-function content_60a39adcb54890_51683897 (Smarty_Internal_Template $_smarty_tpl) {
+function content_60a8d76d50c949_23950291 (Smarty_Internal_Template $_smarty_tpl) {
 $_smarty_tpl->_loadInheritance();
 $_smarty_tpl->inheritance->init($_smarty_tpl, false);
 ?>
@@ -33,24 +33,24 @@ $_smarty_tpl->inheritance->init($_smarty_tpl, false);
 ">
 
         <link rel="stylesheet" href="http://yui.yahooapis.com/pure/0.4.2/pure.css">
-        <link rel="stylesheet" href="<?php echo $_smarty_tpl->tpl_vars['app_url']->value;?>
-/css/main-grid.css">
-        <link rel="stylesheet" href="<?php echo $_smarty_tpl->tpl_vars['app_url']->value;?>
+        <link rel="stylesheet" href="<?php echo $_smarty_tpl->tpl_vars['conf']->value->app_url;?>
+}/css/main-grid.css">
+        <link rel="stylesheet" href="<?php echo $_smarty_tpl->tpl_vars['conf']->value->app_url;?>
 /css/layouts/marketing.css">
-        <link rel="stylesheet" href="<?php echo $_smarty_tpl->tpl_vars['app_url']->value;?>
+        <link rel="stylesheet" href="<?php echo $_smarty_tpl->tpl_vars['conf']->value->app_url;?>
 /css/style.css">
         <?php if ($_smarty_tpl->tpl_vars['hide_intro']->value) {?>
-        <link rel="stylesheet" href="<?php echo $_smarty_tpl->tpl_vars['app_url']->value;?>
+        <link rel="stylesheet" href="<?php echo $_smarty_tpl->tpl_vars['conf']->value->app_url;?>
 /css/style_hide_intro.css">
         <?php }?>
         <link rel="stylesheet" href="http://netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css">
 
         <?php echo '<script'; ?>
- src="<?php echo $_smarty_tpl->tpl_vars['app_url']->value;?>
+ src="<?php echo $_smarty_tpl->tpl_vars['conf']->value->app_url;?>
 /js/jquery.min.js"><?php echo '</script'; ?>
 >
         <?php echo '<script'; ?>
- src="<?php echo $_smarty_tpl->tpl_vars['app_url']->value;?>
+ src="<?php echo $_smarty_tpl->tpl_vars['conf']->value->app_url;?>
 /js/softscroll.js"><?php echo '</script'; ?>
 >
 
@@ -64,9 +64,9 @@ $_smarty_tpl->inheritance->init($_smarty_tpl, false);
                 <a class="pure-menu-heading" href=""><?php echo (($tmp = @$_smarty_tpl->tpl_vars['page_title']->value)===null||$tmp==='' ? "Tytuł domyślny" : $tmp);?>
 </a>
                 <ul>
-                    <li class="pure-menu-selected"><a href="<?php echo $_smarty_tpl->tpl_vars['app_url']->value;?>
+                    <li class="pure-menu-selected"><a href="<?php echo $_smarty_tpl->tpl_vars['conf']->value->app_url;?>
 #app_top">Strona główna</a></li>
-                    <li><a href="<?php echo $_smarty_tpl->tpl_vars['app_url']->value;?>
+                    <li><a href="<?php echo $_smarty_tpl->tpl_vars['conf']->value->app_url;?>
 #app_content">Kalkulator</a></li>
                 </ul>
             </div>
@@ -81,7 +81,7 @@ $_smarty_tpl->inheritance->init($_smarty_tpl, false);
 
                 </p>
                 <p>
-                    <a href="<?php echo $_smarty_tpl->tpl_vars['app_url']->value;?>
+                    <a href="<?php echo $_smarty_tpl->tpl_vars['conf']->value->app_url;?>
 #app_content" class="pure-button pure-button-primary">Kalkulator kredytowy</a>
                 </p>
             </div>
@@ -92,7 +92,7 @@ $_smarty_tpl->inheritance->init($_smarty_tpl, false);
             <div id="app_content" class="content">
 
                 <?php 
-$_smarty_tpl->inheritance->instanceBlock($_smarty_tpl, 'Block_53134723160a39adcb52d44_74968517', 'content');
+$_smarty_tpl->inheritance->instanceBlock($_smarty_tpl, 'Block_59424236660a8d76d509703_40420029', 'content');
 ?>
 
 
@@ -101,7 +101,7 @@ $_smarty_tpl->inheritance->instanceBlock($_smarty_tpl, 'Block_53134723160a39adcb
             <div class="footer l-box is-center">
                 <p>
                     <?php 
-$_smarty_tpl->inheritance->instanceBlock($_smarty_tpl, 'Block_65060468260a39adcb53c02_86218795', 'footer');
+$_smarty_tpl->inheritance->instanceBlock($_smarty_tpl, 'Block_189185330460a8d76d50b674_48374228', 'footer');
 ?>
 
                 </p>
@@ -112,32 +112,28 @@ $_smarty_tpl->inheritance->instanceBlock($_smarty_tpl, 'Block_65060468260a39adcb
     </body>
 </html><?php }
 /* {block 'content'} */
-class Block_53134723160a39adcb52d44_74968517 extends Smarty_Internal_Block
+class Block_59424236660a8d76d509703_40420029 extends Smarty_Internal_Block
 {
 public $subBlocks = array (
   'content' => 
   array (
-    0 => 'Block_53134723160a39adcb52d44_74968517',
+    0 => 'Block_59424236660a8d76d509703_40420029',
   ),
 );
 public function callBlock(Smarty_Internal_Template $_smarty_tpl) {
-?>
- Domyślna treść zawartości .... <?php
 }
 }
 /* {/block 'content'} */
 /* {block 'footer'} */
-class Block_65060468260a39adcb53c02_86218795 extends Smarty_Internal_Block
+class Block_189185330460a8d76d50b674_48374228 extends Smarty_Internal_Block
 {
 public $subBlocks = array (
   'footer' => 
   array (
-    0 => 'Block_65060468260a39adcb53c02_86218795',
+    0 => 'Block_189185330460a8d76d50b674_48374228',
   ),
 );
 public function callBlock(Smarty_Internal_Template $_smarty_tpl) {
-?>
- Domyślna treść stopki .... <?php
 }
 }
 /* {/block 'footer'} */
--- templates/main.tpl
+++ templates/main.tpl
@@ -6,16 +6,16 @@
         <meta name="description" content="{$page_description|default:"Opis domyślny"}">
 
         <link rel="stylesheet" href="http://yui.yahooapis.com/pure/0.4.2/pure.css">
-        <link rel="stylesheet" href="{$app_url}/css/main-grid.css">
-        <link rel="stylesheet" href="{$app_url}/css/layouts/marketing.css">
-        <link rel="stylesheet" href="{$app_url}/css/style.css">
+        <link rel="stylesheet" href="{$conf->app_url}}/css/main-grid.css">
+        <link rel="stylesheet" href="{$conf->app_url}/css/layouts/marketing.css">
+        <link rel="stylesheet" href="{$conf->app_url}/css/style.css">
         {if $hide_intro }
-        <link rel="stylesheet" href="{$app_url}/css/style_hide_intro.css">
+        <link rel="stylesheet" href="{$conf->app_url}/css/style_hide_intro.css">
         {/if}
         <link rel="stylesheet" href="http://netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css">
 
-        <script src="{$app_url}/js/jquery.min.js"></script>
-        <script src="{$app_url}/js/softscroll.js"></script>
+        <script src="{$conf->app_url}/js/jquery.min.js"></script>
+        <script src="{$conf->app_url}/js/softscroll.js"></script>
 
         <title>Kalkulator kredytowy</title>
 
@@ -26,8 +26,8 @@
             <div class="home-menu pure-menu pure-menu-open pure-menu-horizontal pure-menu-fixed">
                 <a class="pure-menu-heading" href="">{$page_title|default:"Tytuł domyślny"}</a>
                 <ul>
-                    <li class="pure-menu-selected"><a href="{$app_url}#app_top">Strona główna</a></li>
-                    <li><a href="{$app_url}#app_content">Kalkulator</a></li>
+                    <li class="pure-menu-selected"><a href="{$conf->app_url}#app_top">Strona główna</a></li>
+                    <li><a href="{$conf->app_url}#app_content">Kalkulator</a></li>
                 </ul>
             </div>
         </div>
@@ -39,7 +39,7 @@
                     {$page_description|default:"Opis domyślny"}
                 </p>
                 <p>
-                    <a href="{$app_url}#app_content" class="pure-button pure-button-primary">Kalkulator kredytowy</a>
+                    <a href="{$conf->app_url}#app_content" class="pure-button pure-button-primary">Kalkulator kredytowy</a>
                 </p>
             </div>
         </div>
@@ -48,13 +48,13 @@
 
             <div id="app_content" class="content">
 
-                {block name=content} Domyślna treść zawartości .... {/block}
+                {block name=content}{/block}
 
             </div>
 
             <div class="footer l-box is-center">
                 <p>
-                    {block name=footer} Domyślna treść stopki .... {/block}
+                    {block name=footer}{/block}
                 </p>
                 <p>Kalkulator kredytowy. © 2014 - Present Yahoo! Inc. All rights reserved.</p>
 
