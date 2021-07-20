c9ce3262a3e3b2222a7c80874986fee37a9d4fb5
lim4k <lim4k@DESKTOP-QIQF5OT>
2021-07-20 19:22:00 +0200

    nowa struktura i rozszerzenia

--- index.php
+++ index.php
@@ -1,7 +1,5 @@
 <?php
 
-require_once dirname(__FILE__) . '/config.php';
 require_once dirname(__FILE__) . '/init.php';
-include $conf->root_path . '/ctrl.php';
 
-include _ROOT_PATH . '/app/calc_cred.php';
+include $conf->root_path . '/ctrl.php';
